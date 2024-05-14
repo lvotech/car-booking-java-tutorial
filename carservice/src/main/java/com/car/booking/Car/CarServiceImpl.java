46003283a6b3135d21c86d1c0a3df169b143c41a
lvotech <168668607+lvotech@users.noreply.github.com>
2024-05-14 17:52:43 +0700

    Update CarServiceImpl.java

--- carservice/src/main/java/com/car/booking/Car/CarServiceImpl.java
+++ carservice/src/main/java/com/car/booking/Car/CarServiceImpl.java
@@ -2,6 +2,8 @@ package com.car.booking.Car;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,8 @@ import com.car.booking.Car.CarException.CarNotFoundException;
 
 @Service
 public class CarServiceImpl implements CarService {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass().toGenericString());
 
     @Autowired
     private CarRepository carRepository;
@@ -20,22 +24,29 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public CarEntity getCarById(Long id) {
+        logger.info("getCarById", id);
         CarEntity carEntity = carRepository.findById(id).orElse(null);
+        logger.info("getCarById", carEntity);
         if (carEntity == null) {
             throw new CarNotFoundException("Car with id " + id + " not found");
         }
-        
+
         return carEntity;
     }
 
     @Override
-    public CarEntity addOrUpdateCar(CarEntity car) {
-        return carRepository.save(car);
+    public CarEntity addOrUpdateCar(CarEntity carEntity) {
+        logger.info("addOrUpdateCar", carEntity);
+        CarEntity carEntityResult = carRepository.save(carEntity);
+        logger.info("addOrUpdateCar - carEntityResult", carEntityResult);
+
+        return carEntity;
     }
 
     @Override
     public void deleteCar(Long id) {
+        logger.info("deleteCar", id);
         carRepository.deleteById(id);
     }
-    
+
 }
